53046b310338519a4d36397a4488f203e85d88e2
Your Name <your-email@example.com>
2026-03-04 00:23:51 +0800

    Fix Turbopack configuration for Next.js 16 build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, // Turn off temporarily to make debugging easier
+  reactStrictMode: false,
   webpack: (config) => {
     config.watchOptions = {
       ignored: [
@@ -11,14 +11,7 @@ const nextConfig = {
     }
     return config
   },
+  turbopack: {},
 }
 
 module.exports = nextConfig
-module.exports = {
-  webpack: (config) => {
-    config.watchOptions = {
-      ignored: /node_modules|System Volume Information|Recovery/,
-    }
-    return config
-  },
-}
